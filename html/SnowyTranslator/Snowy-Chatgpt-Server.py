607574b8dc62fc73a001ec0a8ac44c899cad2dbe
snowysmile <91005170+snowysmile@users.noreply.github.com>
2023-09-07 19:15:49 +0900

    Site updated: 2023-09-07 19:15:44

--- html/SnowyTranslator/Snowy-Chatgpt-Server.py
+++ html/SnowyTranslator/Snowy-Chatgpt-Server.py
@@ -11,9 +11,6 @@ emojis = emoji.UNICODE_EMOJI["en"].keys()
 app = Flask(__name__)
 CORS(app)
 
-api_key = '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'
-openai.api_key = api_key
-
 def dprint(*args, **kwargs):
     if DEBUG_MODE:
         print(*args, **kwargs)
@@ -30,6 +27,10 @@ def chatgpt_emoji():
             "content": "Please translate the input to many many many emojis as many as you can. Do your best with only emojis only."
         }]
         input_text = request.json['text']
+        openai.api_key = request.json['apikey']
+        print("input_text:", input_text)
+        print("openai.api_key:", openai.api_key)
+
         lprint("input_text:", input_text)
         messages.append({"role": "user", "content": input_text})
         response = openai.ChatCompletion.create(
@@ -58,15 +59,15 @@ def chatgpt_emoji():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    LOCAL_TEST = True if 1 else False
-    DEBUG_MODE = True if 0 else False
+    LOCAL_TEST_CHATGPT = True if 1 else False
+    DEBUG_MODE = True if 1 else False
     IS_BANNED = True if 0 else False
 
-    if LOCAL_TEST:
-        print("haha")
+    if LOCAL_TEST_CHATGPT:
+        print("LOCAL_TEST")
         app.run(debug=DEBUG_MODE, threaded=True, port=3002, host='localhost', use_reloader=False)
     else:
-        print("hehe")
+        print("SERVER_RUN")
         current_path = os.path.abspath(__file__)
         current_dir = os.path.dirname(current_path)
         context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
