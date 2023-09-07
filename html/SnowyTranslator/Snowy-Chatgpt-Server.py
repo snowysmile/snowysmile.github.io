ed9bee41eb6e7730cf93a5e8b31c8e0ce6a0a412
snowysmile <91005170+snowysmile@users.noreply.github.com>
2023-09-07 14:38:55 +0900

    Site updated: 2023-09-07 14:38:49

--- html/SnowyTranslator/Snowy-Chatgpt-Server.py
+++ html/SnowyTranslator/Snowy-Chatgpt-Server.py
@@ -3,13 +3,15 @@ from flask_cors import CORS
 import requests
 import openai
 import emoji
+import ssl
+import os
 import re
 emojis = emoji.UNICODE_EMOJI["en"].keys()
 
 app = Flask(__name__)
 CORS(app)
 
-api_key = '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'
+api_key = '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'
 openai.api_key = api_key
 
 def dprint(*args, **kwargs):
@@ -48,13 +50,25 @@ def chatgpt_emoji():
                 emoji_list.append(the_emoji)
         emoji_text = ' '.join(emoji_list)
 
-        return response_text + " \n " + emoji_text
+        # return response_text + " \n " + emoji_text
+        return emoji_text
 
     except Exception as e:
         print('error:', str(e))
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    LOCAL_TEST = True if 0 else False
+    LOCAL_TEST = True if 1 else False
     DEBUG_MODE = True if 0 else False
-    app.run(debug=True, port=3002)
+    IS_BANNED = True if 0 else False
+
+    if LOCAL_TEST:
+        print("haha")
+        app.run(debug=DEBUG_MODE, threaded=True, port=3002, host='localhost', use_reloader=False)
+    else:
+        print("hehe")
+        current_path = os.path.abspath(__file__)
+        current_dir = os.path.dirname(current_path)
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        context.load_cert_chain(current_dir + '/fullchain.pem', current_dir + '/privkey.pem')
+        app.run(debug=DEBUG_MODE, threaded=True, port=3002, host='0.0.0.0', use_reloader=False, ssl_context=context)
