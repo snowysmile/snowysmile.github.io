11ae96c26eac1186d9ff44310a8d2a847e53a3a1
snowysmile <91005170+snowysmile@users.noreply.github.com>
2023-09-27 17:54:20 +0900

    Site updated: 2023-09-27 17:54:14

--- html/SnowyTranslator/Snowy-Chatgpt-Server.py
+++ html/SnowyTranslator/Snowy-Chatgpt-Server.py
@@ -55,53 +55,59 @@ def chatgpt_emoji():
         print('error:', str(e))
         return jsonify({'error': str(e)})
 
-
-@app.route('/chatgpt-cute_japanese', methods=['POST'])
-def chatgpt_cute_japanese():
-    try:
-        input_text = request.json['text']
-        openai.api_key = request.json['apikey']
-        lprint("input_text:", input_text)
-
-        messages = [{
-            "role": "system",
-            "content": "Please translate/convert my input sentence to happy, friendly, and positive Japanese. Make my Japanese grammar correct and don't return extra things."
-        }, {"role": "user", "content": input_text}]
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            temperature=0.8,
-            max_tokens=160,
-        )
-        response_text = response.choices[0].message['content']
-        lprint("response_text:", response_text)
-        lprint("response:", response.usage)
-
-        return response_text
-
-    except Exception as e:
-        print('error:', str(e))
-        return jsonify({'error': str(e)})
-
 @app.route('/chatgpt', methods=['POST'])
 def chatgpt():
     try:
         input_text = request.json['text']
         openai.api_key = request.json['apikey']
         query_type = request.json['query']
+        max_token = 280
+
         lprint("input_text:", input_text)
         lprint("query_type:", query_type)
 
         messages = []
-        if query_type == "related-emojis":
+        if query_type == "cute-japanese":
+            messages.append({
+                "role": "system",
+                "content": "Please translate/convert my input sentence to happy, friendly, and positive Japanese. Make my Japanese grammar correct and don't return extra things."
+            })
+        elif query_type == "related-emojis":
             messages.append({
                 "role": "system",
                 "content": "Please convert input to related emojis, answer me with as many different emojis as you can."
             })
+            max_token = 160
         elif query_type == "sentence-emojis":
             messages.append({
                 "role": "system",
                 "content": "Please add many 顔文字 to my input sentence to make it cute and interesting. Don't do other things."
+            })
+            max_token = 160
+        elif query_type == "translate-ja":
+            messages.append({
+                "role": "system",
+                "content": "You will be provided with statements, and your task is to translate them to decent, natural, and standard Japanese."
+            })
+        elif query_type == "translate-en":
+            messages.append({
+                "role": "system",
+                "content": "You will be provided with statements, and your task is to translate them to decent, natural, and standard English."
+            })
+        elif query_type == "translate-cn":
+            messages.append({
+                "role": "system",
+                "content": "You will be provided with statements, and your task is to translate them to decent, natural, and standard Traditional Chinese."
+            })
+        elif query_type == "cute-ja":
+            messages.append({
+                "role": "system",
+                "content": "You will be provided with statements, and your task is to translate them to cute and natural Japanese."
+            })
+        elif query_type == "lovely-ja":
+            messages.append({
+                "role": "system",
+                "content": "You will be provided with statements, and your task is to translate them to lovely and friendly Japanese with 顔文字. (Don't make the texts much longer than before)"
             })
         else:
             print("wrong query type")
