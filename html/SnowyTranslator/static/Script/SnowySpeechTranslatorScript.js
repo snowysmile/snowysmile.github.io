4729219ce2b7c3a3c683aa8e189cae759ddbcaac
snowysmile <91005170+snowysmile@users.noreply.github.com>
2023-09-28 00:01:48 +0900

    Site updated: 2023-09-28 00:01:41

--- html/SnowyTranslator/static/Script/SnowySpeechTranslatorScript.js
+++ html/SnowyTranslator/static/Script/SnowySpeechTranslatorScript.js
@@ -1,36 +1,30 @@
-  async function ggSpeakUrlGet(text, sourceLanguage) {
-    if (window.location.protocol === "file:") {
-      var ggTranslateUrl = "https://translate.google.com/translate_tts?ie=UTF-8&tl=" +
-        sourceLanguage +  "&client=tw-ob&q=" + encodeURIComponent(text);
-      return ggTranslateUrl;
-    }
-    else {
-      try {
-        var baseUrl;
-        if (window.location.protocol === 'file:') {
-            baseUrl = 'https://snsmile.site:3000';
-        } else {
-            baseUrl = '';
-        }
-        const response = await fetch('${baseUrl}/text-to-speech', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            sourceLanguage: sourceLanguage,
-            text: text
-          })
-        });
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const audioData = await response.arrayBuffer();
-        const audioBlob = new Blob([audioData], { type: 'audio/mpeg' });
-        return URL.createObjectURL(audioBlob);
-      } catch (error) {
-        console.error('Error:', error);
-        return null; // or handle the error in a way appropriate for your application
+async function ggSpeakUrlGet(text, sourceLanguage) {
+  if (window.location.protocol === "file:") {
+    var ggTranslateUrl = "https://translate.google.com/translate_tts?ie=UTF-8&tl=" +
+      sourceLanguage +  "&client=tw-ob&q=" + encodeURIComponent(text);
+    return ggTranslateUrl;
+  }
+  else {
+    try {
+      const response = await fetch('https://snsmile.site:3000/text-to-speech', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          sourceLanguage: sourceLanguage,
+          text: text
+        })
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
+      const audioData = await response.arrayBuffer();
+      const audioBlob = new Blob([audioData], { type: 'audio/mpeg' });
+      return URL.createObjectURL(audioBlob);
+    } catch (error) {
+      console.error('Error:', error);
+      return null; // or handle the error in a way appropriate for your application
     }
   }
+}
