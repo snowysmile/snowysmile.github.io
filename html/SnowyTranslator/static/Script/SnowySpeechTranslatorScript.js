af42b68094f11f18a9400b30070867d202695344
snowysmile <91005170+snowysmile@users.noreply.github.com>
2023-09-27 22:09:02 +0900

    Site updated: 2023-09-27 22:08:55

--- html/SnowyTranslator/static/Script/SnowySpeechTranslatorScript.js
+++ html/SnowyTranslator/static/Script/SnowySpeechTranslatorScript.js
@@ -1,30 +1,32 @@
-async function ggSpeakUrlGet(text, sourceLanguage) {
-  if (window.location.protocol === "file:") {
-    var ggTranslateUrl = "https://translate.google.com/translate_tts?ie=UTF-8&tl=" +
-      sourceLanguage +  "&client=tw-ob&q=" + encodeURIComponent(text);
-    return ggTranslateUrl;
-  }
-  else {
-    try {
-      const response = await fetch('https://snsmile.site:3000/text-to-speech', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          sourceLanguage: sourceLanguage,
-          text: text
-        })
-      });
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
+<script>
+  async function ggSpeakUrlGet(text, sourceLanguage) {
+    if (window.location.protocol === "file:") {
+      var ggTranslateUrl = "https://translate.google.com/translate_tts?ie=UTF-8&tl=" +
+        sourceLanguage +  "&client=tw-ob&q=" + encodeURIComponent(text);
+      return ggTranslateUrl;
+    }
+    else {
+      try {
+        const response = await fetch('https://snsmile.site:3000/text-to-speech', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            sourceLanguage: sourceLanguage,
+            text: text
+          })
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const audioData = await response.arrayBuffer();
+        const audioBlob = new Blob([audioData], { type: 'audio/mpeg' });
+        return URL.createObjectURL(audioBlob);
+      } catch (error) {
+        console.error('Error:', error);
+        return null; // or handle the error in a way appropriate for your application
       }
-      const audioData = await response.arrayBuffer();
-      const audioBlob = new Blob([audioData], { type: 'audio/mpeg' });
-      return URL.createObjectURL(audioBlob);
-    } catch (error) {
-      console.error('Error:', error);
-      return null; // or handle the error in a way appropriate for your application
     }
   }
-}
+</script>
